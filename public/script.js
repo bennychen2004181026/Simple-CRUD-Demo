1b50a0e9c622c8bdc2b41ac6cbb11d308a18dff7
Ben <ben83026@outlook.com>
2023-10-02 21:19:32 +1100

    Add submit course route

--- public/script.js
+++ public/script.js
@@ -1,11 +1,12 @@
+
 document.addEventListener("DOMContentLoaded", () => {
   const addCourseForm = document.getElementById("add-course-form");
   const courseList = document.getElementById("course-list");
   const errorContainer = document.getElementById("error-container");
+  const url ="http://localhost:8000/api/courses/"
 
   const fetchCourses = async () => {
     try {
-      const url = "http://localhost:8000/api/courses/";
       const response = await fetch(url);
       const courses = await response.json();
       console.log("courses", courses);
@@ -14,17 +15,46 @@ document.addEventListener("DOMContentLoaded", () => {
         const li = document.createElement("li");
         li.textContent = `Title:${course.title},Instruction:${course.instructor}. Duration:${course.duration}`;
         // create a delete button'
-        const deleteBtn = document.createElement("button")
-        deleteBtn.textContent = "Delete"
-        deleteBtn.classList.add("delete-button")
-        deleteBtn.dataset.courseId = course._id//data-course-if
-        li.appendChild(deleteBtn)
-        courseList.appendChild(li)
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "Delete";
+        deleteBtn.classList.add("delete-button");
+        deleteBtn.dataset.courseId = course._id; //data-course-if
+        li.appendChild(deleteBtn);
+        courseList.appendChild(li);
       });
     } catch (error) {
       console.error("Error fetching courses", error);
     }
   };
-
+  addCourseForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const formData = new FormData(addCourseForm);
+    const courseData = Object.fromEntries(formData.entries());
+    console.log(courseData);
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(courseData),
+      });
+      // if (!response.ok){}
+      if (response.ok) {
+        addCourseForm.reset();
+        fetchCourses();
+      }
+      const responseData = await response.json();
+      if (responseData.errors) {
+        errorContainer.innerHTML = ""; //clear previous errors
+        for (const field in responseData.errors) {
+          const errorText = responseData.errors(field);
+          const errorElement = document.createElement("p");
+          errorElement.textContent = errorText;
+          errorContainer.appendChild(errorElement);
+        }
+      }
+    } catch (error) {
+      console.error("Error adding course", response);
+    }
+  });
   fetchCourses();
 });
